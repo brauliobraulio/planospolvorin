bf68e0c0d5b2c2cfe210b46c2339a612b55ce439
brauliobraulio <brauliosanhuezaz@gmail.com>
2026-03-22 18:12:54 -0300

    Delete polvorin directory

--- polvorin/src:/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Polvorin from "./Polvorin.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Polvorin />
-  </React.StrictMode>
-);
